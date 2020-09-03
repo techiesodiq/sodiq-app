c8feba1373a15e8fe3cf93271b850891e5508165
Sodiq <sodiq.akanmu001@gmail.com>
2020-09-03 10:37:49 +0100

    Updates

---
+++ precache-manifest.c6869f07ca2259d972f773cbfa915db7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e4d7f4a7a61ef404ea35994d2216c42",
+    "url": "/sodiq-app/index.html"
+  },
+  {
+    "revision": "1141cbac475aedec2d12",
+    "url": "/sodiq-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ac76c954915739bf9d63",
+    "url": "/sodiq-app/static/js/2.4813ca8c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sodiq-app/static/js/2.4813ca8c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1141cbac475aedec2d12",
+    "url": "/sodiq-app/static/js/main.e3a37d27.chunk.js"
+  },
+  {
+    "revision": "c149c31223a6c95fb41b",
+    "url": "/sodiq-app/static/js/runtime-main.3e40c18c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sodiq-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
